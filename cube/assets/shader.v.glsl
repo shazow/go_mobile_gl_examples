e169d660f81508462fe70668963fa830897b0407
Andrey Petrov <andrey.petrov@shazow.net>
2015-07-10 19:37:34 +0200

    Added light, had to backport a bunch of glsl version 150+ helpers.

--- cube/assets/shader.v.glsl
+++ cube/assets/shader.v.glsl
@@ -6,10 +6,16 @@ uniform mat4 model;
 
 attribute vec3 vertCoord;
 attribute vec2 vertTexCoord;
+attribute vec3 vertNormal;
 
+varying vec3 fragCoord;
 varying vec2 fragTexCoord;
+varying vec3 fragNormal;
 
 void main() {
-	fragTexCoord = vertTexCoord;
+    fragCoord = vertCoord;
+    fragTexCoord = vertTexCoord;
+    fragNormal = vertNormal;
+
     gl_Position = projection * view * model * vec4(vertCoord, 1);
 }
--- cube/main.go
+++ cube/main.go
@@ -24,11 +24,15 @@ type Shape struct {
 type Shader struct {
 	program      gl.Program
 	vertCoord    gl.Attrib
+	vertNormal   gl.Attrib
 	vertTexCoord gl.Attrib
 
 	projection gl.Uniform
 	view       gl.Uniform
 	model      gl.Uniform
+
+	lightPosition    gl.Uniform
+	lightIntensities gl.Uniform
 }
 
 type Engine struct {
@@ -48,7 +52,7 @@ func (e *Engine) Start() {
 
 	e.shape.coordsPerVertex = 3
 	e.shape.texCoordsPerVertex = 2
-	e.shape.vertexCount = len(cubeData) / (e.shape.coordsPerVertex + e.shape.texCoordsPerVertex)
+	e.shape.vertexCount = len(cubeData) / (e.shape.coordsPerVertex + e.shape.texCoordsPerVertex + 3)
 
 	e.shape.buf = gl.CreateBuffer()
 	gl.BindBuffer(gl.ARRAY_BUFFER, e.shape.buf)
@@ -60,6 +64,9 @@ func (e *Engine) Start() {
 	e.shader.projection = gl.GetUniformLocation(e.shader.program, "projection")
 	e.shader.view = gl.GetUniformLocation(e.shader.program, "view")
 	e.shader.model = gl.GetUniformLocation(e.shader.program, "model")
+
+	e.shader.lightIntensities = gl.GetUniformLocation(e.shader.program, "lightIntensities")
+	e.shader.lightPosition = gl.GetUniformLocation(e.shader.program, "lightPosition")
 
 	e.shape.texture, err = LoadTexture("gopher.png")
 	if err != nil {
@@ -110,14 +117,22 @@ func (e *Engine) Draw(c event.Config) {
 	m = mgl.HomogRotate3D(float32(since.Seconds()), mgl.Vec3{0, 1, 0})
 	gl.UniformMatrix4fv(e.shader.model, m[:])
 
+	// Light
+	gl.Uniform3fv(e.shader.lightIntensities, []float32{0, 1, 0})
+	gl.Uniform3fv(e.shader.lightPosition, []float32{0, 0, 0})
+
 	// Draw our shape
 	gl.BindBuffer(gl.ARRAY_BUFFER, e.shape.buf)
 
+	stride := 4 * (3 + 2 + 3) // 4 bytes in float, (3+2+3) values per vertex
 	gl.EnableVertexAttribArray(e.shader.vertCoord)
-	gl.VertexAttribPointer(e.shader.vertCoord, e.shape.coordsPerVertex, gl.FLOAT, false, 20, 0) // 4 bytes in float, 5 values per vertex
+	gl.VertexAttribPointer(e.shader.vertCoord, e.shape.coordsPerVertex, gl.FLOAT, false, stride, 0)
 
 	gl.EnableVertexAttribArray(e.shader.vertTexCoord)
-	gl.VertexAttribPointer(e.shader.vertTexCoord, e.shape.texCoordsPerVertex, gl.FLOAT, false, 20, 12)
+	gl.VertexAttribPointer(e.shader.vertTexCoord, e.shape.texCoordsPerVertex, gl.FLOAT, false, stride, 12)
+
+	gl.EnableVertexAttribArray(e.shader.vertNormal)
+	gl.VertexAttribPointer(e.shader.vertNormal, 3, gl.FLOAT, false, stride, 20)
 
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindTexture(gl.TEXTURE_2D, e.shape.texture)
